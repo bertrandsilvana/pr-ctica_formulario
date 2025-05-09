38df3d7e712181e925b98d2021123772abc9834a
bertrandsilvana <silvanamarilin79@gmail.com>
2025-05-09 20:32:49 -0300

    correccion

--- Resumen.js
+++ Resumen.js
@@ -10,11 +10,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const color = params.get("color");
     const comentarios = params.get("comentarios");
     const intereses = params.getAll("intereses");
+
+      console.log("Resumen de datos:");
+      console.log(`Nombre: ${nombre}`);
+      console.log(`Edad: ${edad}`);
+      console.log(`Fecha de nacimiento: ${fecha}`);
+      console.log(`País: ${pais}`);
+      console.log(`Género: ${genero}`);
+      console.log(`Color favorito: ${color}`);
+      console.log(`Comentarios: ${comentarios}`);
+      console.log(`Intereses: ${intereses.join(', ')}`);
   
     if (nombre) {
       const contenedor = document.getElementById("root");
       contenedor.innerHTML = `
-        <h2>Resumen de datos:</h2>
+        <h1>Resumen de datos:</h1>
         <p><strong>Nombre:</strong> ${nombre}</p>
         <p><strong>Edad:</strong> ${edad}</p>
         <p><strong>Fecha de nacimiento:</strong> ${fecha}</p>
